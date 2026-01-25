4ec0f480008ef36628afd1c417e0e25f65bcfa73
dashigara <205404502+dashigara@users.noreply.github.com>
2026-01-25 14:57:02 +0900

    install用スクリプト修正

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -1,13 +1,21 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
-if (!Test-Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1) {
-    if (!Test-Path $env:USERPROFILE\Documents\PowerShell\) {
-        New-Item -ItemType Directory $env:USERPROFILE\Documents\PowerShell
-    }
-    New-Item -ItemType SymbolicLink -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value .\pwsh\Microsoft.PowerShell_profile.ps1
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) {
+    Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs
+    exit 
 }
-if (!Test-Path $env:USERPROFILE\.posh\config.yaml) {
-    if (!Test-Path $env:USERPROFILE\.posh\) {
-        New-Item -ItemType Directory $env:USERPROFILE\.posh
+
+function  New-ConfigLink {
+    param($targetPath, $fileName)
+    
+    if (!(Test-Path $targetPath)) {
+        New-Item -ItemType Directory $targetPath
     }
-    New-Item -ItemType SymbolicLink -Path $env:USERPROFILE\.posh -Value .\pwsh\config.yaml
+    
+    if (!(Test-Path $targetPath\$fileName)) {
+        New-Item -ItemType SymbolicLink -Path $targetPath\$fileName -Value $PSScriptRoot\$fileName
+    }
 }
+
+@(
+    @{ targetPath = "$env:USERPROFILE\Documents\PowerShell"; fileName = "Microsoft.PowerShell_profile.ps1" }
+    @{ targetPath = "$env:USERPROFILE\.posh"; fileName = "config.yaml" }
+) | ForEach-Object { New-ConfigLink @_ }
