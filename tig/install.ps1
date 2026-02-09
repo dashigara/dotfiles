f09d28c1ec378930849844c53c4ad14b34b49b40
dashigara <205404502+dashigara@users.noreply.github.com>
2026-02-09 21:45:22 +0900

    tig

---
+++ tig/install.ps1
@@ -0,0 +1,20 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) {
+    Start-Process powershell.exe "-ExecutionPolicy RemoteSigned -File `"$PSCommandPath`"" -Verb RunAs
+    exit 
+}
+
+function  New-ConfigLink {
+    param($targetPath, $fileName)
+    
+    if (!(Test-Path $targetPath)) {
+        New-Item -ItemType Directory $targetPath
+    }
+    
+    if (!(Test-Path $targetPath\$fileName)) {
+        New-Item -ItemType SymbolicLink -Path $targetPath\$fileName -Value $PSScriptRoot\$fileName
+    }
+}
+
+@(
+    @{ targetPath = "$env:USERPROFILE"; fileName = ".tigrc" }
+) | ForEach-Object { New-ConfigLink @_ }
