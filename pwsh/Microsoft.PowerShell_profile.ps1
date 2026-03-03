318df62b4d83c64dda950e7f39717f5598f7a7a5
dashigara <205404502+dashigara@users.noreply.github.com>
2026-03-03 21:43:55 +0900

    zoxide setup add.

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,3 @@
 oh-my-posh init pwsh --config $env:USERPROFILE/.posh/config.yaml | Invoke-Expression
 Set-Alias -Name which -Value where.exe
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
