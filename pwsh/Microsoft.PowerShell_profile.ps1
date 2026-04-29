310a9b0f08bb363866986daa28642fc4a01a2252
dashigara <205404502+dashigara@users.noreply.github.com>
2026-04-30 07:59:29 +0900

    explorer alias

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
 oh-my-posh init pwsh --config $env:USERPROFILE/.posh/config.yaml | Invoke-Expression
-Set-Alias -Name which -Value where.exe
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+Set-Alias -Name which -Value where.exe
+
+function excur() { explorer . }
+Set-Alias -Name e -Value excur
+
