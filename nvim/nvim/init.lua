85795a2f0731ffeb96464cd6e586efa35d1c7b2f
dashigara <205404502+dashigara@users.noreply.github.com>
2026-07-14 01:35:02 +0900

    tabstop by lang

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -30,10 +30,10 @@ end
 -- treesitter
 vim.treesitter.start = (function(wrapped)
     return function(bufnr, lang)
-	    lang = lang or vim.fn.getbufvar(bufnr or '', '&filetype')
-	    pcall(wrapped, bufnr, lang)
+        lang = lang or vim.fn.getbufvar(bufnr or "", "&filetype")
+        pcall(wrapped, bufnr, lang)
     end
-    end)(vim.treesitter.start)
+end)(vim.treesitter.start)
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.wo.foldmethod = "expr"
 
@@ -58,6 +58,19 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 -- 行頭でのTab文字の表示幅
 vim.opt.shiftwidth = 4
+-- Filetype毎のタブ幅
+local filetype_tabstop = { lua = 2, markdown = 2, javascript = 2, typesript = 2 } -- filetype毎のインデント幅
+local usrftcfg = vim.api.nvim_create_augroup("UserFileTypeConfig", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    group = usrftcfg,
+    callback = function(args)
+        local ftts = filetype_tabstop[args.match]
+        if ftts then
+            vim.bo.tabstop = ftts
+            vim.bo.shiftwidth = ftts
+        end
+    end,
+})
 
 -- ====================
 -- 検索設定
